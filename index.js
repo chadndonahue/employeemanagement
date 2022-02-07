166074970ecd38053266ee41dbe74baf8c8a5dcb
Chad <chadndonahue@gmail.com>
2022-02-06 19:07:01 -0500

    setup up sql connection and added more dependencies

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const inquirer = require("inquirer")
-const mysql = require("mysql2")
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+require("console.table");
+const { printTable } = require("console-table-printer");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Lassie110437a",
+    database: "employee_db",
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    startPrompt();
+});
